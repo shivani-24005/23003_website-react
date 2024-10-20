19971d1dee3658656017d7ca4365a8de46370d46
sravya Ramayanam <divyaramayanam126@gmail.com>
2024-10-20 14:54:08 +0530

    first commit

--- react_basics/vite.config.js
+++ react_basics/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/  221FA23001_SHOPPING_WEBSITE-react/',
+  plugins: {outDir: 'dist'},
 })
+
