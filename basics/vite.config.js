32e2bdd4fa4b4a86dc906e49ec453406bdff6e41
sravya Ramayanam <divyaramayanam126@gmail.com>
2024-10-22 00:02:26 +0530

    first commit

--- basics/vite.config.js
+++ basics/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/sustainable_website-react/",
+  base:"/23003_website-react/",
 })
